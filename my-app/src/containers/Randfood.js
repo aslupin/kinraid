d177a62f5c87854e3980f67d790d00a0fcfde3b1
poonsht <poon_arsene_lupin@hotmail.com>
2018-04-02 23:32:02 +0700

    clear sometags

---
+++ my-app/src/containers/Randfood.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from '../logo.svg';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App-randfood">
+        <p>Poonsht</p>
+      </div>
+    );
+  }
+}
+
+export default App;
