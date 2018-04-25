c8d2a862c55554218b73959456f2d7aad24c000a
poonsht <poon_arsene_lupin@hotmail.com>
2018-04-25 21:54:27 +0700

    fix: color-a-style

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -5,10 +5,13 @@ const Footer = (props) => {
        paddingTop: '20px',
        fontSize: '11px'
    }
+   const alink = {
+       color: '#cca300'
+   }
     return(
         <div style={styP}>
             <p >This project is part of the Innovative Thinking (01200101) <br/> 
-            Used data <a href="https://gist.github.com/aslupin/c33d3b86bb084aaeb93d6044e688ff87">@Gist</a>   |Follow us <a href="https://github.com/aslupin/kinraid">@Github</a></p>
+            Used data <a href="https://gist.github.com/aslupin/c33d3b86bb084aaeb93d6044e688ff87" style={alink} >@Gist</a>   |Follow us <a href="https://github.com/aslupin/kinraid" style={alink} >@Github</a></p>
         </div>
     );
 }
